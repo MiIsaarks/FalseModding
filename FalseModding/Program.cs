a3292677e8301c25484c73d36692032097d88f1a
Samuel <samuca6307@gmail.com>
2026-07-14 23:46:57 -0300

    Adicionar arquivos de projeto.

---
+++ FalseModding/Program.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using RoR2;
+using EntityStates;
+using UnityEngine;
+
+namespace FalseSonTweak
+{
+    [BepInPlugin("com.YourName.FalseSonDamageScaling", "FalseSonDamageScaling", "1.0.0")]
+    public class MainPlugin : BaseUnityPlugin
+    {
+        private static float? originalDamage = null;
+        public void Awake()
+        {
+
+            On.RoR2.FalseSonController.GetGrowthLaserBonusDuration += (orig, self) =>
+            {
+                return 0f;
+            };
+
+           
+            On.EntityStates.FalseSon.LaserFatherCharged.OnEnter += (orig, self) =>
+            {
+                if(originalDamage == null) 
+                {
+                    originalDamage = EntityStates.FalseSon.LaserFatherCharged.damageCoefficient;
+                }
+           
+                var growthController = self.characterBody.GetComponent<RoR2.FalseSonController>();
+                if (growthController != null)
+                {
+                    int currentGrowth = growthController.growthLevel;
+
+                   
+                    float damageMultiplier = 1f + (currentGrowth * 0.1f);
+
+                    
+                    EntityStates.FalseSon.LaserFatherCharged.damageCoefficient =originalDamage.Value * damageMultiplier;
+                }
+
+               
+                orig(self);
+            };
+
+           
+        }
+    }
+}
