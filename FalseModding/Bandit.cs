1e049dc2a96c3fc488e35727f5b5856df29819ef
Samuel de Moraes Barbosa <samuca6307@gmail.com>
2026-07-29 15:54:46 -0300

    resets cooldowns on crit

--- FalseModding/Bandit.cs
+++ FalseModding/Bandit.cs
@@ -3,51 +3,68 @@ using RoR2.Skills;
 using UnityEngine.AddressableAssets;
 using R2API;
 using EntityStates.Bandit2.Weapon;
+using UnityEngine;
 
 namespace ForgottenSkillsTweaks
 {
+    
     public static class Bandit
     {
-        public static bool isBleeding = false;
+        private static SkillLocator skillLocator = null;
+        private static bool IsRevolver = false;
         public static void init()
         {
+            On.RoR2.HealthComponent.TakeDamage += HealthComponent_TakeDamage;
+            GlobalEventManager.onServerDamageDealt += ServerDamageDealt;
+
             On.EntityStates.Bandit2.Weapon.BaseFireSidearmRevolverState.OnEnter += (orig, self) =>
             {
                 if (self is FireSidearmResetRevolver)
                 {
-                    isBleeding = true;
-                    On.RoR2.HealthComponent.TakeDamage += HealthComponent_TakeDamage;
+                    IsRevolver = true;
+                   
+                    skillLocator = self.characterBody.skillLocator;
                 }
                 orig(self);
              
             };
 
-            
+        }
 
+
+        
+
+        private static void ServerDamageDealt(DamageReport damageReport)
+        {
+            DamageInfo damageinfo = damageReport.damageInfo;
+
+            if (damageinfo.crit && IsRevolver)
+            {
+                skillLocator.GetSkill(SkillSlot.Primary).Reset();
+                skillLocator.GetSkill(SkillSlot.Secondary).Reset();
+                skillLocator.GetSkill(SkillSlot.Utility).Reset();
+                
+
+            }
         }
 
         private static void HealthComponent_TakeDamage(On.RoR2.HealthComponent.orig_TakeDamage orig, HealthComponent self, DamageInfo damageInfo)
         {
-            if (isBleeding)
-            {
+           
                 if (self.body && self.body.HasBuff(RoR2Content.Buffs.SuperBleed))
                 {
-                    // Multiply damage (e.g., 50% extra damage against Hemorrhaged targets)
-                    damageInfo.damage *= 2.0f;
-                    isBleeding = false;
+                    if (IsRevolver)
+                    {
+                        damageInfo.damage *= 2.5f;
+                    IsRevolver = false;
+                    }
                 }
 
                 orig(self, damageInfo);
 
                 GlobalEventManager.instance.OnHitEnemy(damageInfo, self.gameObject);
                 GlobalEventManager.instance.OnHitAll(damageInfo, self.gameObject);
-            }
-            else
-            {
-                orig(self, damageInfo);
-            }
-           
-           
+          
         }
        
     
