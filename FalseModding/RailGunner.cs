1ecf265e0c04edce83fc6fd0878b6c48b6db864b
Samuel de Moraes Barbosa <samuca6307@gmail.com>
2026-08-03 19:47:44 -0300

    RailGunner chnages can be disabled in settings

--- FalseModding/RailGunner.cs
+++ FalseModding/RailGunner.cs
@@ -11,49 +11,55 @@ namespace ForgottenSkillsTweaks
     public static class RailGunner
     {
         private static float consecutiveWeakSpotHit = 0;
+        private static SkillDef SlowMine;
+        private static SkillDef HH44;
+
+        private static string originalHH44Desc;
+        private static string[] originalHH44Desc2;
         public static void init()
         {
-            SkillDef SlowMine = Addressables.LoadAssetAsync<SkillDef>("RoR2/DLC1/Railgunner/RailgunnerBodyFireMineBlinding.asset").WaitForCompletion();
+             SlowMine = Addressables.LoadAssetAsync<SkillDef>("RoR2/DLC1/Railgunner/RailgunnerBodyFireMineBlinding.asset").WaitForCompletion();
 
-            SkillDef HH44 = Addressables.LoadAssetAsync<SkillDef>("RoR2/DLC1/Railgunner/RailgunnerBodyScopeLight.asset").WaitForCompletion();
+             HH44 = Addressables.LoadAssetAsync<SkillDef>("RoR2/DLC1/Railgunner/RailgunnerBodyScopeLight.asset").WaitForCompletion();
 
-            SlowMine.baseRechargeInterval = 8f;
-            SlowMine.baseMaxStock = 2;
+            originalHH44Desc = HH44.skillDescriptionToken;
+            originalHH44Desc2 = HH44.keywordTokens;
+
+           
 
             LanguageAPI.Add("HH44NewDescription",
                "Activate your <style=cIsUtility>short-range scope</style>, highlighting <style=cIsdamage>Weak Points</style> and transforming your weapon into a quick <style=cIsdamage>500% damage</style> railgun. Consecutive <style=cIsdamage>Weak Point</style> hits increases damage");
 
-            HH44.skillDescriptionToken = "HH44NewDescription";
+            
 
             LanguageAPI.Add("KEYWORD_CONSECUTIVE", "<style=cKeywordName>Sniper's Flow</style><style=cSub>Consecutive <style=cIsDamage>Weak Point</style> hits boost the damage of your next shot by <style=cIsdamage>100%</style> per consecutive hit up to 10 times.</style>");
 
+            config.OnConfigChanged += UpdateSkills;
 
-            HH44.keywordTokens = new string[]
-            {
-                "KEYWORD_WEAKPOINT",
-                "KEYWORD_CONSECUTIVE"
-            };
+          
             On.EntityStates.Railgunner.Weapon.BaseFireSnipe.OnEnter += (orig, self) =>
             {
-             
 
-                if(self is FireSnipeLight)
+                if (config.HH44.Value)
                 {
-                    self.damageCoefficient = 5f + consecutiveWeakSpotHit;
+                    if (self is FireSnipeLight)
+                    {
+                        self.damageCoefficient = 5f + consecutiveWeakSpotHit;
 
-                   
+
+                    }
+
+                    if (self is FireSnipeCryo)
+                    {
+                        self.damageCoefficient += consecutiveWeakSpotHit;
+                    }
+
+                    if (self is FireSnipeSuper)
+                    {
+                        self.damageCoefficient += consecutiveWeakSpotHit;
+                    }
                 }
-
-                if(self is FireSnipeCryo)
-                {
-                    self.damageCoefficient += consecutiveWeakSpotHit;
-                }
-
-                if(self is FireSnipeSuper)
-                {
-                    self.damageCoefficient += consecutiveWeakSpotHit;
-                }
-
+       
                 orig(self);
             };
 
@@ -73,6 +79,35 @@ namespace ForgottenSkillsTweaks
             };
 
          
+        }
+
+        private static void UpdateSkills()
+        {
+            if (config.HH44.Value)
+            {
+                HH44.skillDescriptionToken = "HH44NewDescription";
+                HH44.keywordTokens = new string[]
+                {
+                    "KEYWORD_WEAKPOINT",
+                    "KEYWORD_CONSECUTIVE"
+                };
+            }
+            else
+            {
+                HH44.skillDescriptionToken = originalHH44Desc;
+                HH44.keywordTokens = originalHH44Desc2;
+            }
+            if (config.PolarMines.Value)
+            {
+                SlowMine.baseRechargeInterval = 8f;
+                SlowMine.baseMaxStock = 2;
+
+            }
+            else
+            {
+                SlowMine.baseRechargeInterval = 12f;
+                SlowMine.baseMaxStock = 1;
+            }
         }
     }
 }
